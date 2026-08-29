bd0f563bdfbf05bea3fe6a8599ef2c60fec7fb36
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-08-29 23:48:24 +0700

    update HomeScreen to use FlatList for rendering posts and add additional mock posts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
 import AppHeader from "@/components/AppHeader";
 import PostCard from "@/components/PostCard";
-import { COLORS, SPACING } from "@/constants/theme";
-import { Alert, StyleSheet, View } from "react-native";
+import { COLORS } from "@/constants/theme";
+import { StyleSheet, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { MOCK_POSTS } from "../data/mock-data";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <AppHeader/>
-      <PostCard post={MOCK_POSTS[0]}/>
+      <AppHeader />
+      <FlatList
+        data={MOCK_POSTS}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <PostCard post={item} />}
+        showsVerticalScrollIndicator={true}
+      />
     </SafeAreaView>
   );
 }
@@ -18,8 +23,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.background,
-  },
-  content: {
-    padding: SPACING.lg,
   },
 });
