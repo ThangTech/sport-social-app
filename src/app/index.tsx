52a5e3db02839fbf130e48bc03221c7e74d25c7b
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-08-29 16:59:40 +0700

    remove Expo starter components"

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+
 import { Button, Text, View } from "react-native";
 
 
@@ -6,7 +6,6 @@ import { Button, Text, View } from "react-native";
 export default function HomeScreen() {
   return (
      <View>
-       <Header/>
       <Text>Hello World</Text>
       <Text>Ứng dụng quản lý chung cư mini</Text>
       <Button title="Bắt đầu" onPress={() => alert("Xin chào mobile")}/>
