44922642cad277a838626d3eb288b47cfb8b7e35
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-09-06 19:23:44 +0700

    Add Entity Framework Core packages and UserStatus enum; remove unused Class1

---
+++ backend/src/SocialSport.Domain/Enums/UserStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialSport.Domain.Enums
+{
+    public enum UserStatus
+    {
+        Active = 1,
+        Suspended = 2,
+        Banned = 3,
+        Deactivated = 4
+    }
+}
--- backend/src/SocialSport.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SocialSport.Infrastructure;
-
-public class Class1
-{
-
-}
